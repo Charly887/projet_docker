24d9810de847a553e47a0fc57d358912d2aed777
Charly.kouemekokaze <charly@DESKTOP-607MBIN>
2023-12-15 14:36:14 +0100

    projetRegistry

---
+++ Registry/Registry/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Registry.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Sex { get; set; }
+        public int Age { get; set; }
+    }
+}
